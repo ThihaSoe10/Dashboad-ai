e80116520b87cf83abe3d95425c90b20f4d513bf
ThihaSoe10 <thihasoe102000@gmail.com>
2024-07-28 22:33:02 +0630

    third

--- src/dash/Dashboard.js
+++ src/dash/Dashboard.js
@@ -20,6 +20,13 @@ const Dashboard = () => {
 
   const totalUsers = userData ? Object.keys(userData).length : 0;
 
+  // Calculate total profit per hour and divide by 18000
+  const totalProfitPerHour = userData
+    ? Object.keys(userData).reduce((total, userId) => {
+        return total + userData[userId].autoIncrement * 3600;
+      }, 0) / 18000
+    : 0;
+
   // Sort user data by profit per hour in descending order
   const sortedUserData = userData
     ? Object.keys(userData).sort((a, b) => {
@@ -34,7 +41,10 @@ const Dashboard = () => {
       <h1>User Data</h1>
       {userData ? (
         <>
-          <p className="total-users">Total Users: {totalUsers}</p>
+          <div className="summary">
+            <p className="total-users">Total Users: {totalUsers}</p>
+            <p className="total-profit">Total Coin: {totalProfitPerHour.toFixed(2)}</p>
+          </div>
           <div className="table-container">
             <table>
               <thead>
